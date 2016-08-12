7b95b277f8e2d1174ad9b1976e137ba3691432e6
jihad khorfan <jihad.kherfan@gmail.com>
2016-08-12 21:26:19 +0200

    call API

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,62 @@
 /**
  * Created by jihad.kherfan on 6/10/2016.
  */
-$(document).ready(function(){
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var lat= position.coords.latitude ;
-      var lon= position.coords.longitude;
-      var appID= 'de0d2ebeb48a8dd2459c399e78b745ca' ;
-      $.getJSON("api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID="+appID, function(json) {
-        $("#cityName").text(json.name);
-      })
-    });
+var unitC = true;
+
+
+$(document).ready(function() {
+  function positionC () {
+    if (Navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+      });
+    } else {
+      loadWeather("Homs, SY", "");
+    }
   }
+  positionC();
+  setInterval(positionC, 600000);// update weather every 10 minutes
+  $('.unit').click(function(){
+    alert('hello');
+    if(unitC === true){
+      unitC = false;
+      positionC();
+    } else {
+      unitC = true;
+      positionC();
+    }
+  })
 });
+function loadWeather(location,woeid){
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: function(){
+      if(unitC === true){
+        return 'c';
+      } else {
+        return 'f';
+      }
+    },
+    success: function (weather) {
+      var city = weather.city;
+      var high = weather.high;
+      var low = weather.low;
+      var wcode = '<img class ="weathericon" src="img/'+ weather.code +'.png">';
+      document.title = city+':'+ weather.temp;
+      $('#cityName').text(city);
+
+      $('#dayTemp').text(high);
+
+
+      $('#nightTemp').text(low);
+
+
+      $('.weather-icon').html(wcode);
+    }
+  })
+}
+
 
 
 /* $.getJSON("/json/cats.json", function(json) {
