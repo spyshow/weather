c4a303fa5e3403a6cd5e2295042fc073918455dd
jihad khorfan <jihad.kherfan@gmail.com>
2016-08-29 09:13:44 +0200

    V0.1

--- js/custom.js
+++ js/custom.js
@@ -3,18 +3,19 @@
  */
 var unitC = true;
 var ctx = $("#myChart");
-function loadWeather(location,woeid){
+
+function loadWeather(location, woeid) {
   $.simpleWeather({
     location: location,
     woeid: woeid,
-    unit: (function(){
-      if(unitC === true){
+    unit: (function() {
+      if (unitC === true) {
         return 'c';
       } else {
         return 'f';
       }
     })(),
-    success: function (weather) {
+    success: function(weather) {
 
       // start chart
 
@@ -22,56 +23,55 @@ function loadWeather(location,woeid){
         type: 'line',
         data: {
           showScale: false,
-          labels: [weather.forecast[0].day,weather.forecast[1].day, weather.forecast[2].day, weather.forecast[3].day, weather.forecast[4].day],
-          datasets: [
-            {
-              label: "low temp",
-              fill: true,
-              lineTension: 0.1,
-              backgroundColor: "#85ECFF",
-              borderColor: "#85ECFF",
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.0,
-              borderJoinStyle: 'miter',
-              pointBorderColor: "#85ECFF",
-              pointBackgroundColor: "#85ECFF",
-              pointBorderWidth: 5,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: "white",
-              pointHoverBorderColor: "white",
-              pointHoverBorderWidth: 2,
-              pointRadius: 1,
-              pointHitRadius: 10,
-              data: [weather.forecast[0].low,weather.forecast[1].low, weather.forecast[2].low, weather.forecast[3].low, weather.forecast[4].low],
-              spanGaps: false,
+          labels: [weather.forecast[0].day, weather.forecast[1].day, weather.forecast[2].day, weather.forecast[3].day, weather.forecast[4].day],
+          datasets: [{
+            label: "low temp",
+            fill: true,
+            lineTension: 0.1,
+            backgroundColor: "#85ECFF",
+            borderColor: "#85ECFF",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "#85ECFF",
+            pointBackgroundColor: "#85ECFF",
+            pointBorderWidth: 5,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "white",
+            pointHoverBorderColor: "white",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [weather.forecast[0].low, weather.forecast[1].low, weather.forecast[2].low, weather.forecast[3].low, weather.forecast[4].low],
+            spanGaps: false,
 
-            },{
-              label: "high temp",
-              fill: true,
-              lineTension: 0.1,
-              backgroundColor: "white",
-              borderColor: "white",
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.0,
-              borderJoinStyle: 'miter',
-              pointBorderColor: "#85ECFF",
-              pointBackgroundColor: "#85ECFF",
-              pointBorderWidth: 1,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: "#85ECFF",
-              pointHoverBorderColor: "#85ECFF",
-              pointHoverBorderWidth: 2,
-              pointRadius: 1,
-              pointHitRadius: 10,
-              data: [weather.forecast[0].high , weather.forecast[1].high, weather.forecast[2].high, weather.forecast[3].high, weather.forecast[4].high],
-              spanGaps: false,
+          }, {
+            label: "high temp",
+            fill: true,
+            lineTension: 0.1,
+            backgroundColor: "white",
+            borderColor: "white",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "#85ECFF",
+            pointBackgroundColor: "#85ECFF",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "#85ECFF",
+            pointHoverBorderColor: "#85ECFF",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [weather.forecast[0].high, weather.forecast[1].high, weather.forecast[2].high, weather.forecast[3].high, weather.forecast[4].high],
+            spanGaps: false,
 
-            }
+          }
 
           ]
-        },// end of data
+        }, // end of data
 
         options: {
           title: {
@@ -81,17 +81,16 @@ function loadWeather(location,woeid){
             display: false
           },
           scales: {
-            showScale: false,
+
             yAxes: [{
-              display: false,
-              color : '#f39c12',
+
+              color: '#f39c12',
               ticks: {
-                beginAtZero:true
+                beginAtZero: true
               }
             }]
           }
-        }// end of option
-
+        } // end of option
 
       });
 
@@ -100,26 +99,44 @@ function loadWeather(location,woeid){
       var city = weather.city;
       var high = weather.high;
       var low = weather.low;
-      var wcode = '<img class ="weathericon" src="img/'+ weather.code +'.png">';
-      document.title = city+':'+ weather.temp;
+      var wcode = '<i class="wi wi-yahoo-' + weather.code + '"></i>';
+      document.title = city + ':' + weather.temp;
       $('#cityName').text(city);
-      $('#dayTemp').html(high+'<span class="temp">'+weather.units.temp+'</span>');
-      $('#nightTemp').html(low+'<span class="temp">'+weather.units.temp+'</span>');
+      $('#dayTemp').html(high + '<span class="temp">' + weather.units.temp + '</span>');
+      $('#nightTemp').html(low + '<span class="temp">' + weather.units.temp + '</span>');
       $('.weather-icon').html(wcode);
-      if((weather.temp > 25 && weather.units.temp === 'C') || (weather.temp > 77 && weather.units.temp === 'F')) {
-        $('.weather-board').animate({backgroundColor: '#f1c40f'}, 1500);
-        $('.outer-1').animate({borderColor: '#f39c12'}, 100);
-        $('.outer-1').animate({backgroundColor: '#f39c12'}, 100);
-        $('.inner-1').animate({backgroundColor: '#e67e22'}, 100);
-        $('.inner-1').animate({borderColor: '#e67e22'}, 100);
-        $('.inner-2').animate({backgroundColor: '#d35400'}, 100);
-        $('.inner-2').animate({borderColor: '#d35400'}, 100);
-        $('#nightTemp').animate({color: '#f39c12'}, 100);
-        $('#nightTemp').css('text-shadow','0 0 8px #f39c12');
-        $(".my-btn:hover").css("color","#f39c12");
-        $('.my-btn:active').css("color","#f39c12");
+      if ((weather.temp > 25 && weather.units.temp === 'C') || (weather.temp > 77 && weather.units.temp === 'F')) {
+        $('.weather-board').animate({
+          backgroundColor: '#f1c40f'
+        }, 1500);
+        $('.outer-1').animate({
+          borderColor: '#f39c12'
+        }, 100);
+        $('.outer-1').animate({
+          backgroundColor: '#f39c12'
+        }, 100);
+        $('.inner-1').animate({
+          backgroundColor: '#e67e22'
+        }, 100);
+        $('.inner-1').animate({
+          borderColor: '#e67e22'
+        }, 100);
+        $('.inner-2').animate({
+          backgroundColor: '#d35400'
+        }, 100);
+        $('.inner-2').animate({
+          borderColor: '#d35400'
+        }, 100);
+        $('#nightTemp').animate({
+          color: '#f39c12'
+        }, 100);
+        $('#nightTemp').css('text-shadow', '0 0 8px #f39c12');
+        $(".my-btn:hover").css("color", "#f39c12");
+        $('.my-btn:active').css("color", "#f39c12");
       } else {
-        $('.weather-board').animate({backgroundColor: '#C8F7FF'}, 1500);
+        $('.weather-board').animate({
+          backgroundColor: '#C8F7FF'
+        }, 1500);
       }
       myChart.update();
     }
@@ -128,9 +145,10 @@ function loadWeather(location,woeid){
 
 $(document).ready(function() {
 
-  function positionC () {
+  function positionC() {
+
     if (Navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
+      navigator.geolocation.getCurrentPosition(function(position) {
         loadWeather(position.coords.latitude + ',' + position.coords.longitude);
       });
     } else {
@@ -138,23 +156,18 @@ $(document).ready(function() {
     }
   }
   positionC();
-  setInterval(loadWeather, 600000);// update weather every 10 minutes
-  $('.unit').click(function(){
-    if(unitC === true){
+  setInterval(loadWeather, 600000); // update weather every 10 minutes
+  $('.unit').click(function() {
+    if (unitC === true) {
       unitC = false;
-
       positionC();
-
     } else {
       unitC = true;
-      $(".Chart").html('');
       positionC();
-
     }
   })
 
 });
-
 
 /* $.getJSON("/json/cats.json", function(json) {
  $(".message").html(JSON.stringify(json));
